da908e1a1189c11e4bf9b184d8187da8848e6dd7
SinaRiyahi <sina.riyahi76@gmail.com>
2024-01-07 23:25:22 +0330

    Create Class Articles

---
+++ Domain/Entities/Blog/ArticleAttachment.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Blog
+{
+    [Table("ArticleAttachments", Schema = "Blog")]
+    public class ArticleAttachment
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public int ArticleId { get; set; }
+        public virtual Article Article { get; set; }
+
+        [MaxLength(128)]
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+        public string FileContentType { get; set; }
+        public string FileSize { get; set; }
+       // public FileType FileType { get; set; }
+        public string FilePath { get; set; }
+        public DateTime CreateDate { get; set; }
+
+    }
+}
