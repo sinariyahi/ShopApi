564af3280e81b38021823bb00d604a0568ef355d
SinaRiyahi <sina.riyahi76@gmail.com>
2024-01-10 23:58:16 +0330

    Create Class Entities

---
+++ Domain/Entities/Base/NewsLetter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Base
+{
+    [Table("NewsLetters", Schema = "Base")]
+    public class NewsLetter
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Template { get; set; }
+        public string Remark { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+    }
+}
