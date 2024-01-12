f65051e295647a55eb84716a2975fca8764a6722
SinaRiyahi <sina.riyahi76@gmail.com>
2024-01-12 23:59:16 +0330

    Create Class Entities

---
+++ Domain/Entities/Base/DeliveryLocation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Base
+{
+    [Table("DeliveryLocations", Schema = "Base")]
+
+    public class DeliveryLocation
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(64)]
+        public string Title { get; set; }
+        [MaxLength(512)]
+        public string Remark { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
