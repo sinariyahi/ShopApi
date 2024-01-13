96d32ef00d64614881a62edd9bec85807ef91616
SinaRiyahi <sina.riyahi76@gmail.com>
2024-01-13 23:58:14 +0330

    Create Class Entities

---
+++ Domain/Entities/Base/DataEntryHistory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Base
+{
+    [Table("DataEntryHistories", Schema = "Base")]
+    public class DataEntryHistory
+    {
+        public int Id { get; set; }
+        public Guid Code { get; set; }
+        public DateTime CreateDate { get; set; }
+        public Guid? UserId { get; set; }
+       // public virtual User User { get; set; }
+        public bool? IsAdminUser { get; set; }
+    }
+}
