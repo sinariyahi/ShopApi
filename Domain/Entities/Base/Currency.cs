e74a04fc703288e3c27dea775295e3b828c1c5ca
SinaRiyahi <sina.riyahi76@gmail.com>
2024-01-14 23:56:25 +0330

    Create Class Entities

---
+++ Domain/Entities/Base/Currency.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Base
+{
+    [Table("Currencies", Schema = "Base")]
+    public class Currency
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(64)]
+        public string Title { get; set; }
+        [MaxLength(8)]
+        public string Symbol { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
