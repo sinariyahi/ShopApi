fa99462f31dd9489b1e8697cd03836aa2b435a08
SinaRiyahi <sina.riyahi76@gmail.com>
2024-01-16 23:47:40 +0330

    Create Class Entities

---
+++ Domain/Entities/Base/City.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Base
+{
+
+    [Table("Cities", Schema = "Base")]
+    public class City
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(256)]
+        public string Title { get; set; }
+
+      //  public Province ProvinceId { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
