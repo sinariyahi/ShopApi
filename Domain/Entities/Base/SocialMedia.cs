b28de1f9c3c08d45fab5900649bff1741a79cfc7
SinaRiyahi <sina.riyahi76@gmail.com>
2024-01-17 23:55:49 +0330

    Create Class Entities

---
+++ Domain/Entities/Base/SocialMedia.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Base
+{
+    [Table("SocialMedias", Schema = "Base")]
+    public class SocialMedia
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(256)]
+        public string Title { get; set; }
+        public bool IsActive { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string Link { get; set; }
+//        public virtual ICollection<SocialMediaAttachment> SocialMediaAttachments { get; set; }
+        public SocialMedia()
+        {
+ //           SocialMediaAttachments = new HashSet<SocialMediaAttachment>();
+        }
+
+    }
+
+
+}
