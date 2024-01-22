16bc7f684db1c6e89e0752af179766d6d12eff50
SinaRiyahi <sina.riyahi76@gmail.com>
2024-01-22 23:54:39 +0330

    Create Class Entities

---
+++ Domain/Entities/Catalog/FavoriteProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Catalog
+{
+    [Table("FavoriteProducts", Schema = "Catalog")]
+    public class FavoriteProduct
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+       // public virtual User User { get; set; }
+        public int ProductId { get; set; }
+     //   public virtual Product Product { get; set; }
+        public DateTime CreateDate { get; set; }
+
+    }
+}
