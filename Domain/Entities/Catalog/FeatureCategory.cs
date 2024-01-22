29130081c7ca9b0eaff10e316dddae04bdcca97a
SinaRiyahi <sina.riyahi76@gmail.com>
2024-01-22 23:57:26 +0330

    Create Class Entities

---
+++ Domain/Entities/Catalog/FeatureCategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Catalog
+{
+    [Table("FeatureCategories", Schema = "Catalog")]
+    public class FeatureCategory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(256)]
+        public string Title { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
