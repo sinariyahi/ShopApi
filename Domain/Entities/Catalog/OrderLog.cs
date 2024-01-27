609585bb7ad543aaf3495b86455421c2c6061121
SinaRiyahi <sina.riyahi76@gmail.com>
2024-01-27 21:22:11 +0330

    Create Class Entities

---
+++ Domain/Entities/Catalog/OrderLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Catalog
+{
+    [Table("OrderLogs", Schema = "Catalog")]
+    public class OrderLog
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid OrderId { get; set; }
+        public virtual Order Order { get; set; }
+      //  public OrderStatus OrderStatus { get; set; }
+        public string OrderStatusRemark { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+
+    }
+}
