e712fc0a59ae08ca829c5cb66acaacbfea6ebaf9
SinaRiyahi <sina.riyahi76@gmail.com>
2024-01-30 23:53:41 +0330

    Create Class Entities

---
+++ Domain/Entities/Catalog/ProductUsage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Catalog
+{
+    [Table("ProductUsages", Schema = "Catalog")]
+    public class ProductUsage
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(128)]
+        public string Title { get; set; }
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
