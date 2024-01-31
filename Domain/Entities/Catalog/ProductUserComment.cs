870c33f56830740f64c8f4ee826cc4e577881736
SinaRiyahi <sina.riyahi76@gmail.com>
2024-01-31 22:31:36 +0330

    Create Class Entities

---
+++ Domain/Entities/Catalog/ProductUserComment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Catalog
+{
+    [Table("ProductUserComments", Schema = "Catalog")]
+    public class ProductUserComment
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+        public Guid? UserId { get; set; }
+      //  public virtual User User { get; set; }
+        [MaxLength(1024)]
+        public string Comment { get; set; }
+        public DateTime CreateDate { get; set; }
+        public bool ShowInSite { get; set; } = false;
+    }
+}
