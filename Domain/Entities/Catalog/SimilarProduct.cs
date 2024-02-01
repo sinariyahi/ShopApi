2e82c8826a4850e82adddb33107ec0f1debb76f8
SinaRiyahi <sina.riyahi76@gmail.com>
2024-02-01 22:21:52 +0330

    Create Class Entities

---
+++ Domain/Entities/Catalog/SimilarProduct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Catalog
+{
+    [Table("SimilarProducts", Schema = "Catalog")]
+    public class SimilarProduct
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        public int SimilarId { get; set; }
+        public virtual Product Similar { get; set; }
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+        public string Remark { get; set; }
+
+
+    }
+}
