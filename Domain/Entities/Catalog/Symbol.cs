4ceb4063ae1e34caadae6238882c7b1da5efbb10
SinaRiyahi <sina.riyahi76@gmail.com>
2024-02-02 21:31:18 +0330

    Create Class Entities

---
+++ Domain/Entities/Catalog/Symbol.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Catalog
+{
+    [Table("Symbols", Schema = "Catalog")]
+    public class Symbol
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(256)]
+        public string Title { get; set; }
+        public int? ParentId { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
