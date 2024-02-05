77816f10c8f4cd78d95c82c41b25eb5c6cc69651
SinaRiyahi <sina.riyahi76@gmail.com>
2024-02-05 21:20:48 +0330

    Create Class Entities

---
+++ Domain/Entities/Media/SliderAttachment.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Media
+{
+    [Table("SliderAttachments", Schema = "Media")]
+    public class SliderAttachment
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public int SliderId { get; set; }
+        public virtual Slider Slider { get; set; }
+
+     //   public Device Device { get; set; }
+        [MaxLength(128)]
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+        public string FileContentType { get; set; }
+        public string FileSize { get; set; }
+       // public FileType FileType { get; set; }
+        public string FilePath { get; set; }
+        public DateTime CreateDate { get; set; }
+
+    }
+}
