d75e5cebffedb64246f7d8ce0e4c7b0abf2483a8
SinaRiyahi <sina.riyahi76@gmail.com>
2024-02-07 21:03:20 +0330

    Create Class Entities

---
+++ Domain/Entities/Media/VideoProduct.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities.Catalog;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Media
+{
+    [Table("VideoProducts", Schema = "Media")]
+    public class VideoProduct
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+        public int VideoId { get; set; }
+        public virtual Video Video { get; set; }
+
+
+    }
+}
