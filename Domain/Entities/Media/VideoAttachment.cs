ada1a947a138a2edb3f460d2dd047d2c2ec58a78
SinaRiyahi <sina.riyahi76@gmail.com>
2024-02-07 20:59:24 +0330

    Create Class Entities

---
+++ Domain/Entities/Media/VideoAttachment.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Media
+{
+    [Table("VideoAttachments", Schema = "Media")]
+    public class VideoAttachment
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public int VideoId { get; set; }
+        public virtual Video Video { get; set; }
+
+        [MaxLength(128)]
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+        public string FileContentType { get; set; }
+        public string FileSize { get; set; }
+     //   public FileType FileType { get; set; }
+        public string FilePath { get; set; }
+        public DateTime CreateDate { get; set; }
+
+    }
+}
