2104bbda704b86d56808e64aac1e2f0833db311a
SinaRiyahi <sina.riyahi76@gmail.com>
2024-02-07 21:00:36 +0330

    Create Class Entities

---
+++ Domain/Entities/Media/VideoCategory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Media
+{
+    [Table("VideoCategories", Schema = "Media")]
+    public class VideoCategory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(256)]
+        public string Title { get; set; }
+
+        public bool IsActive { get; set; }
+        public int? SortOrder { get; set; }
+
+        public string Remark { get; set; }
+    }
+}
