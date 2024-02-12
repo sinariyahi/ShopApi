9ebfc130ca56e5e3adeef432fcf6ec234ad7842e
SinaRiyahi <sina.riyahi76@gmail.com>
2024-02-12 23:15:11 +0330

    Create Class Entities

---
+++ Domain/Entities/Security/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Security
+{
+    [Table("Menus", Schema = "Security")]
+    public class Menu
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(256)]
+        public string Title { get; set; }
+        [MaxLength(256)]
+
+        public string Icon { get; set; }
+        [MaxLength(256)]
+        public string Path { get; set; }
+        public int? ParentId { get; set; }
+        public int SortOrder { get; set; }
+
+        public bool IsActive { get; set; }
+        [MaxLength(128)]
+        public string Tag { get; set; }
+    }
+}
