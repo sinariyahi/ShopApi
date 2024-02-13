81e759180e45119f37041713aa147590c140b669
SinaRiyahi <sina.riyahi76@gmail.com>
2024-02-13 21:22:05 +0330

    Create Class Entities

---
+++ Domain/Entities/Security/MenuRole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Security
+{
+    [Table("MenuRoles", Schema = "Security")]
+    public class MenuRole
+    {
+        public int Id { get; set; }
+        public int MenuId { get; set; }
+        public int RoleId { get; set; }
+        public Menu Menu { get; set; }
+       // public Role Role { get; set; }
+    }
+}
