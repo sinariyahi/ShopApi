9470960819db177afac46fa8c3717893070054c4
SinaRiyahi <sina.riyahi76@gmail.com>
2024-02-17 23:03:30 +0330

    Create Class Entities

---
+++ Domain/Entities/Security/SmsLog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Security
+{
+
+    [Table("SmsLogs", Schema = "Security")]
+    public class SmsLog
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public DateTime ActionTime { get; set; }
+        public bool Status { get; set; }
+
+        // public SmsType SmsType { get; set; }
+    }
+}
