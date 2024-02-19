6ebe2ed8a079949b9100e9d86ab6147de44833dc
SinaRiyahi <sina.riyahi76@gmail.com>
2024-02-19 21:41:31 +0330

    Create Class Entities

---
+++ Domain/Entities/Security/UserDiscipline.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Security
+{
+    [Table("UserDisciplines", Schema = "Security")]
+    public class UserDiscipline
+    {
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+
+        public int DisciplineId { get; set; }
+        public virtual Role Discipline { get; set; }
+    }
+}
