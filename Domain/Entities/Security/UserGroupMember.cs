6ec37103292b185c09c0a9d2bfd1d2c6bbd70393
SinaRiyahi <sina.riyahi76@gmail.com>
2024-02-21 23:43:59 +0330

    Create Class Entities

--- Domain/Entities/Security/UserGroupMember.cs
+++ Domain/Entities/Security/UserGroupMember.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Entities.Security
 {
-    internal class UserGroupMember
+    [Table("UserGroupMembers", Schema = "Security")]
+    public class UserGroupMember
     {
+        public Guid Id { get; set; }
+
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+
+        public Guid UserGroupId { get; set; }
+        public UserGroup UserGroup { get; set; }
+        //public virtual ICollection<UserGroupAction> UserGroupActions { get; set; }
+
+        //public UserGroupMember()
+        //{
+        //    UserGroupActions = new HashSet<UserGroupAction>();
+        //}
+
     }
 }
