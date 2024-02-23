bcbe088bb4287ecc9572b55067e0f799d7b3ef30
SinaRiyahi <sina.riyahi76@gmail.com>
2024-02-23 23:45:55 +0330

    Create Class Entities

---
+++ Domain/Entities/Security/UserProject.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Security
+{
+    [Table("UserProjects", Schema = "Security")]
+    public class UserProject
+    {
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+    }
+}
