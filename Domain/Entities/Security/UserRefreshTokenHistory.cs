85a8b30aede0ce32a2f0b4dcdb698fb9c37a51bf
SinaRiyahi <sina.riyahi76@gmail.com>
2024-02-26 23:53:47 +0330

    Create Class Entities

---
+++ Domain/Entities/Security/UserRefreshTokenHistory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Security
+{
+    [Table("UserRefreshTokenHistories", Schema = "Security")]
+    public class UserRefreshTokenHistory
+    {
+
+        public int Id { get; set; }
+
+        public DateTime CreateDate { get; set; }
+        public bool IsValid { get; set; }
+        public string RefreshToken { get; set; }
+        public Guid UserId { get; set; }
+        public string UserIP { get; set; }
+        public string UserBrowser { get; set; }
+        public User User { get; set; }
+
+    }
+}
