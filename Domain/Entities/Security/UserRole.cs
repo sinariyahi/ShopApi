e79da8832563b34fda5973b24c4a2cf670dda30b
SinaRiyahi <sina.riyahi76@gmail.com>
2024-02-28 23:46:11 +0330

    Create Class Entities

---
+++ Domain/Entities/Security/UserRole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Security
+{
+    [Table("UserRole", Schema = "Security")]
+    public class UserRole
+    {
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+    }
+}
