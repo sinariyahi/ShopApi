caa9bf35f543da7a386dc0773620401db361f772
SinaRiyahi <sina.riyahi76@gmail.com>
2024-02-29 23:31:35 +0330

    Create Class Entities

---
+++ Domain/Entities/Support/CompanyQuestion.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities.Security;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Support
+{
+    [Table("CompanyQuestions", Schema = "Support")]
+
+    public class CompanyQuestion
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+       // public CompanyQuestionStatus CompanyQuestionStatus { get; set; }
+        public byte[] AttachmentFile { get; set; }
+     //   public ICollection<UserAnswer> UserAnswers { get; set; }
+        public DateTime CreateDate { get; set; }
+        public Guid CreateUserId { get; set; }
+        public virtual User CreateUser { get; set; }
+    }
+}
