2169cac98a44346c819afb4e8882c76231512ce2
SinaRiyahi <sina.riyahi76@gmail.com>
2024-03-04 23:53:32 +0330

    Create Class Entities

---
+++ Domain/Entities/Support/UserLetMeKnow.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Entities.Catalog;
+using Domain.Entities.Security;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Support
+{
+    [Table("UserLetMeKnows", Schema = "Support")]
+    public class UserLetMeKnow
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Mobile { get; set; }
+
+        public int ProductId { get; set; }
+
+        public Product Product { get; set; }
+
+        public Guid? UserId { get; set; }
+        public virtual User User { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+
+    }
+
+}
