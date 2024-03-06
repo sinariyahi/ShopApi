157554dfa72539c2a9a08f78de5eec907dc352c2
SinaRiyahi <sina.riyahi76@gmail.com>
2024-03-06 23:40:02 +0330

    Create Class Entities

---
+++ Domain/Entities/Support/UserNewsLetter.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities.Security;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Support
+{
+    [Table("UserNewsLetters", Schema = "Support")]
+    public class UserNewsLetter
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public Guid? UserId { get; set; }
+        public virtual User User { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+
+    }
+
+}
