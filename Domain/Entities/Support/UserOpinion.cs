0850525f90a66a4e6f7a1c9d00587b83c2d0cb71
SinaRiyahi <sina.riyahi76@gmail.com>
2024-03-06 23:41:21 +0330

    Create Class Entities

---
+++ Domain/Entities/Support/UserOpinion.cs
@@ -0,0 +1,61 @@
+﻿using Domain.Entities.Blog;
+using Domain.Entities.Catalog;
+using Domain.Entities.Security;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Support
+{
+    [Table("UserOpinions", Schema = "Support")]
+    public class UserOpinion
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid? SenderUserId { get; set; }
+        public virtual User SenderUser { get; set; }
+        public int? ProductId { get; set; }
+        public virtual Product Product { get; set; }
+
+
+        public int? ArticleId { get; set; }
+        public virtual Article Article { get; set; }
+
+
+        [Required, MaxLength(512)]
+        public string Remark { get; set; }
+     //   public UserOpinionType UserOpinionType { get; set; }
+
+       // public ShowStatus ShowStatus { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int? Score { get; set; }
+
+        public virtual ICollection<UserOpinionDetail> UserOpinionDetails { get; set; }
+
+        public UserOpinion()
+        {
+            UserOpinionDetails = new HashSet<UserOpinionDetail>();
+
+        }
+    }
+    [Table("UserOpinionDetails", Schema = "Support")]
+
+    public class UserOpinionDetail
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid UserOpinionId { get; set; }
+
+        public virtual UserOpinion UserOpinion { get; set; }
+
+        //public UseOpinionStatus UseOpinionStatus { get; set; }
+        [Required, MaxLength(512)]
+        public string Remark { get; set; }
+    }
+}
