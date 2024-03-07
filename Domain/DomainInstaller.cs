66fac9210df892d8f90430a092f2edd9f20e428b
SinaRiyahi <sina.riyahi76@gmail.com>
2024-03-07 23:54:24 +0330

    Create Class DomainInstaller

---
+++ Domain/DomainInstaller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public static class DomainInstaller
+    {
+        //public static IServiceCollection AddCore(this IServiceCollection services)
+        //{
+        //    var sp = services.BuildServiceProvider();
+        //    Configs configs = sp.GetService<IOptions<Configs>>().Value;
+        //    services.AddDbContext<BIContext>((Action<DbContextOptionsBuilder>)(options =>
+        //    {
+        //        options.UseSqlServer((string)configs.DBConnection);
+        //    }));
+
+        //    return services;
+        //}
+    }
+}
