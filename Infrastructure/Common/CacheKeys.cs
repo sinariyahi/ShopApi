99f36dc38bb247b08a5fe2d53800284de7a374ef
SinaRiyahi <sina.riyahi76@gmail.com>
2024-03-09 22:56:08 +0330

    Create Class Common

---
+++ Infrastructure/Common/CacheKeys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Common
+{
+    public static class CacheKeys
+    {
+        public static string OrganizationUnits = "OrganizationUnits";
+    }
+}
