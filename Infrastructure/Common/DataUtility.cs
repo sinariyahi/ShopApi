72f7b9270aa536e35a5514e7a88cada847a4bab3
SinaRiyahi <sina.riyahi76@gmail.com>
2024-03-10 23:53:06 +0330

    Create Class Utility

---
+++ Infrastructure/Common/DataUtility.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Common
+{
+    public static class DataUtility
+    {
+        public static string CheckNullExcelData(this object data)
+        {
+            return (data == null && data.ToString().Length == 0 && data.ToString() != @"""" && data.ToString() != @"''") ? "-" : data.ToString().Trim();
+        }
+
+        public static string RemoveDashForTitle(string Title)
+        {
+            var str = Title.Replace('-', ' ');
+
+            return str;
+        }
+
+
+        public static int? GetFeatureNumberValue(string data)
+        {
+            int result;
+            if (int.TryParse(data, out result))
+            {
+                return result;
+            }
+            return null;
+        }
+    }
+}
