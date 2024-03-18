20974bf91dc19fd6eed305d2250227e8fc1e8389
SinaRiyahi <sina.riyahi76@gmail.com>
2024-03-19 00:03:29 +0330

    Create Class ImageUtility

---
+++ Infrastructure/Common/ImageUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Common
+{
+    public static class ImageUtility
+    {
+        public static string ConvertToBase64String(byte[] image)
+        {
+            return "data:image/png;base64," + Convert.ToBase64String(image);
+        }
+    }
+}
