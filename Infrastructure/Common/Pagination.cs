c5152979fce79d3f92b79d3151fc45421b823aef
SinaRiyahi <sina.riyahi76@gmail.com>
2024-03-22 23:53:29 +0330

    Create Class Pagination

---
+++ Infrastructure/Common/Pagination.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Common
+{
+    public class Pagination
+    {
+        public int PageNumber { get; set; }
+        public int PageItemCount { get; set; } = 25;
+        public int CurrentPage { get; set; } = 1;
+        public bool WithPagination { get; set; }
+        public int TotalRows { get; set; }
+    }
+}
