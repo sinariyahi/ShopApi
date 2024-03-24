2e9d2b6c9ff63e19fe18ca6fef50d353f079669a
SinaRiyahi <sina.riyahi76@gmail.com>
2024-03-24 23:52:24 +0330

    Create Class RandomImage

---
+++ Infrastructure/Common/RandomImage.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Common
+{
+    public class RandomImage
+    {
+        //Default Constructor 
+        public RandomImage() { }
+        //property
+        public string Text
+        {
+            get { return text; }
+        }
+        public Bitmap Image
+        {
+            get { return image; }
+        }
+        public int Width
+        {
+            get { return width; }
+        }
+        public int Height
+        {
+            get { return height; }
+        }
+        //Private variable
+        private string text;
+        private int width;
+        private int height;
+        private Bitmap image;
+        private Random random = new Random();
+        //Methods declaration
+        public RandomImage(string s, int width, int height)
+        {
+            text = s;
+            SetDimensions(width, height);
+            GenerateImage();
+        }
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+            Dispose(true);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+                image.Dispose();
+        }
+        private void SetDimensions(int width, int height)
+        {
+            if (width <= 0)
+                throw new ArgumentOutOfRangeException("width", width,
+                    "Argument out of range, must be greater than zero.");
+            if (height <= 0)
+                throw new ArgumentOutOfRangeException("height", height,
+                    "Argument out of range, must be greater than zero.");
+            this.width = width;
+            this.height = height;
+        }
+        private void GenerateImage()
+        {
+            Bitmap bitmap = new Bitmap
+              (width, height, PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(bitmap);
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            Rectangle rect = new Rectangle(0, 0, width, height);
+            HatchBrush hatchBrush = new HatchBrush(HatchStyle.SmallConfetti,
+                Color.LightGray, Color.White);
+            g.FillRectangle(hatchBrush, rect);
+            SizeF size;
+            float fontSize = rect.Height + 1;
+            Font font;
+
+            do
+            {
+                fontSize--;
+                font = new Font(FontFamily.GenericSansSerif, fontSize, FontStyle.Bold);
+                size = g.MeasureString(text, font);
+            } while (size.Width > rect.Width);
+            StringFormat format = new StringFormat();
+            format.Alignment = StringAlignment.Center;
+            format.LineAlignment = StringAlignment.Center;
+            GraphicsPath path = new GraphicsPath();
+            //path.AddString(this.text, font.FontFamily, (int) font.Style, 
+            //    font.Size, rect, format);
+            path.AddString(text, font.FontFamily, (int)font.Style, 25, rect, format);
+            float v = 4F;
+            PointF[] points =
+            {
+                new PointF(random.Next(rect.Width) / v, random.Next(
+                   rect.Height) / v),
+                new PointF(rect.Width - random.Next(rect.Width) / v,
+                    random.Next(rect.Height) / v),
+                new PointF(random.Next(rect.Width) / v,
+                    rect.Height - random.Next(rect.Height) / v),
+                new PointF(rect.Width - random.Next(rect.Width) / v,
+                    rect.Height - random.Next(rect.Height) / v)
+          };
+            Matrix matrix = new Matrix();
+            matrix.Translate(0F, 0F);
+            path.Warp(points, rect, matrix, WarpMode.Perspective, 0F);
+            hatchBrush = new HatchBrush(HatchStyle.Percent10, Color.Black, Color.SkyBlue);
+            g.FillPath(hatchBrush, path);
+            int m = Math.Max(rect.Width, rect.Height);
+            for (int i = 0; i < (int)(rect.Width * rect.Height / 30F); i++)
+            {
+                int x = random.Next(rect.Width);
+                int y = random.Next(rect.Height);
+                int w = random.Next(m / 50);
+                int h = random.Next(m / 50);
+                g.FillEllipse(hatchBrush, x, y, w, h);
+            }
+            font.Dispose();
+            hatchBrush.Dispose();
+            g.Dispose();
+            image = bitmap;
+        }
+    }
+}
