0cc3be569a8b286be6d8dd5062dc878534d18b2e
SinaRiyahi <sina.riyahi76@gmail.com>
2024-03-25 21:59:06 +0330

    Create Class RegexUtility

---
+++ Infrastructure/Common/RegexUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Common
+{
+    public static class RegexUtility
+    {
+        public static bool IsValidPhoneNumber(this string input)
+        {
+            string pattern = @"^09[0|1|2|3][0-9]{8}$";
+            Regex reg = new Regex(pattern);
+            return reg.IsMatch(input);
+        }
+        public static bool IsValidEmail(this string input)
+        {
+            string pattern = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
+            Regex reg = new Regex(pattern);
+            return reg.IsMatch(input);
+        }
+
+    }
+}
