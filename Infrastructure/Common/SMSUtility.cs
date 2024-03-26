f26cc812363eceafe45ff020b82fbd66b80f6b0e
SinaRiyahi <sina.riyahi76@gmail.com>
2024-03-26 19:32:16 +0330

    Create Class SMSUtility

---
+++ Infrastructure/Common/SMSUtility.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Common
+{
+    public class SMSUtility
+    {
+        private readonly Configs configs;
+        public SMSUtility(IOptions<Configs> options)
+        {
+            this.configs = options.Value;
+        }
+        public async Task<string> Send(string mobile, string message)
+        {
+            // Credentials
+            string username = configs.SMSUserName;
+            string password = configs.SMSPassword;
+            string domain = configs.SMSDomain;
+            string smsNumber = configs.SMSNumber;
+
+            // Client
+            using var client = new HttpClient();
+
+            // Call
+            var resultCode = await client.GetStringAsync("https://sms.magfa.com/api/http/sms/v1?service=enqueue&username=" +
+                username + "&password=" + password + "&domain=" + domain + $"&from={smsNumber}&to={mobile}&text={message}");
+            return resultCode;
+        }
+    }
+}
