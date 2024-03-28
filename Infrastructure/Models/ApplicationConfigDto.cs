1e64d6daa8cb07f3bc5b0218c19d39ee1c1eb534
SinaRiyahi <sina.riyahi76@gmail.com>
2024-03-28 21:51:10 +0330

    Creat Class ApplicationConfig

---
+++ Infrastructure/Models/ApplicationConfigDto.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models
+{
+    public class ApplicationConfigDto
+    {
+        public int Id { get; set; }
+        public string ApplicationTitle { get; set; }
+        public string ApplicationTitleEn { get; set; }
+        public string MainLogoBase64 { get; set; }
+        public string MainLogoBase64En { get; set; }
+        public IFormFile MainLogo { get; set; }
+        public IFormFile MainLogoEn { get; set; }
+        public string SidebarLogoBase64 { get; set; }
+        public string SidebarLogoBase64En { get; set; }
+        public IFormFile SidebarLogo { get; set; }
+        public IFormFile SidebarLogoEn { get; set; }
+        public bool? ShowFooterEn { get; set; } = false;
+        public bool ShowFooter { get; set; } = false;
+        public string FooterEn { get; set; }
+        public string Footer { get; set; }
+        public int CurrentYear { get; set; }
+    }
+}
