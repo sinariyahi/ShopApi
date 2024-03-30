e59601f3dbe0f9704c50a6a0752f50f0d2ae09d8
SinaRiyahi <sina.riyahi76@gmail.com>
2024-03-30 23:20:47 +0330

    Create Class Step

---
+++ Infrastructure/Models/StepDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models
+{
+    public class StepModel
+    {
+        public object CurrentStep { get; set; }
+
+        public List<StepDto> ListSteps { get; set; }
+    }
+
+    public class StepDto
+    {
+        public object Value { get; set; }
+
+        public string Status { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public string Icon { get; set; }
+
+    }
+
+}
