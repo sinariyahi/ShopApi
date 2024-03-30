bf757d7575a7e1726f85a619c5ded36ca166ed78
SinaRiyahi <sina.riyahi76@gmail.com>
2024-03-30 23:18:47 +0330

    Create Class Menu

---
+++ Infrastructure/Models/MenuDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models
+{
+    public class MenuDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Icon { get; set; }
+        public string To { get; set; }
+        public int? ParentId { get; set; }
+        public string _tag { get; set; }
+        public List<MenuDto>? _children { get; set; }
+
+    }
+}
