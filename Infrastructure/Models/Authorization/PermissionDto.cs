34c1fc8157e4507e5c6e879196cd1aa482fe6fb3
SinaRiyahi <sina.riyahi76@gmail.com>
2024-03-31 23:38:01 +0330

    Create Class PermissionDto

---
+++ Infrastructure/Models/Authorization/PermissionDto.cs
@@ -0,0 +1,38 @@
+﻿using Infrastructure.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Authorization
+{
+    public class PermissionDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public ActionType ActionType { get; set; }
+        public string Url { get; set; }
+        public bool ShowInSidebar { get; set; }
+        public bool ShowInDashboard { get; set; }
+        public int? SortOrder { get; set; }
+        public string GroupName { get; set; }
+        public bool Checked { get; set; }
+    }
+
+    public class PermissionGroupDto
+    {
+        public int Id { get; set; }
+        public string GroupName { get; set; }
+        public int? SortOrder { get; set; }
+        public IEnumerable<PermissionDto> Permissions { get; set; }
+    }
+
+    public class RolePermissionInputModel
+    {
+        public int RoleId { get; set; }
+        public List<int> Permissions { get; set; }
+    }
+}
