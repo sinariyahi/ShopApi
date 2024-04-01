f91e6591469d910350c3aec63d721d1c4fa39019
SinaRiyahi <sina.riyahi76@gmail.com>
2024-04-01 22:15:30 +0330

    Create Class City

---
+++ Infrastructure/Models/Base/CityDto.cs
@@ -0,0 +1,35 @@
+﻿using Infrastructure.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Base
+{
+    public class CityDto
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public bool IsActive { get; set; }
+        public string IsActiveTitle { get; set; }
+        public string ProvinceTitle { get; set; }
+        public Province ProvinceId { get; set; }
+
+    }
+
+
+    public class CityInputModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Province ProvinceId { get; set; }
+        public string ProvinceTitle { get; set; }
+
+        public bool IsActive { get; set; }
+        public string IsActiveTitle { get; set; }
+
+    }
+}
