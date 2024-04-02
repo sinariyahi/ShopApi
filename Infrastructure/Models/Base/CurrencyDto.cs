42b5272d9872dd484f9d05cac22f68ccf65acff5
SinaRiyahi <sina.riyahi76@gmail.com>
2024-04-02 22:51:14 +0330

    Create Class CurrencyDto

---
+++ Infrastructure/Models/Base/CurrencyDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Base
+{
+    public class CurrencyDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Symbol { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
