6e6235c2bea922d2833b808847de243ebf936b14
SinaRiyahi <sina.riyahi76@gmail.com>
2024-04-03 23:52:23 +0330

    Create Class DeliveryLocationDto

---
+++ Infrastructure/Models/Base/DeliveryLocationDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Base
+{
+    public class DeliveryLocationDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Remark { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
