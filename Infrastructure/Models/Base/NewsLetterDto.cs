db9b2bd8c47210f83c98d22cab0ed44ad4c7a622
SinaRiyahi <sina.riyahi76@gmail.com>
2024-04-06 23:28:27 +0330

    Create Class NewsLetterDto

---
+++ Infrastructure/Models/Base/NewsLetterDto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Base
+{
+    public class NewsLetterDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Template { get; set; }
+        public string Remark { get; set; }
+        public string IsActiveTitle { get; set; }
+        public bool IsActive { get; set; }
+
+        public string CreateDate { get; set; }
+
+    }
+    public class UserRegisterNewsLetterDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+        public string CreateDate { get; set; }
+
+    }
+
+
+    public class UserNewsLetterDto
+    {
+        public string Email { get; set; }
+        public string Captcha { get; set; }
+        public string CaptchaKey { get; set; }
+    }
+
+}
