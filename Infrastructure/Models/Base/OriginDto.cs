34f9c9598b6068a454c0274c8e2903a2beca0801
SinaRiyahi <sina.riyahi76@gmail.com>
2024-04-06 23:29:28 +0330

    OriginDto

---
+++ Infrastructure/Models/Base/OriginDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Base
+{
+    public class OriginDto
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public bool IsActive { get; set; }
+        public string IsActiveTitle { get; set; }
+
+    }
+}
