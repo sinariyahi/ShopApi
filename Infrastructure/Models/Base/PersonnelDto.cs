beacb1a98ca399241b0f99940a828e75495e6fad
SinaRiyahi <sina.riyahi76@gmail.com>
2024-04-07 23:56:08 +0330

    Create Class PersonnelDto

---
+++ Infrastructure/Models/Base/PersonnelDto.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructure.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Base
+{
+    public class PersonnelDto
+    {
+        public int Id { get; set; }
+        public string NationalCode { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public Sex Sex { get; set; }
+        public Guid DataEntryHistoryCode { get; set; }
+    }
+
+}
