80dcd7279a18a489740c2970a6685b12fbb3f5c6
SinaRiyahi <sina.riyahi76@gmail.com>
2024-04-08 23:57:40 +0330

    Create Class SocialMediaDto

---
+++ Infrastructure/Models/Base/SocialMediaDto.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Base
+{
+    public class SocialMediaDto
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Link { get; set; }
+
+        public bool IsActive { get; set; }
+        public string IsActiveTitle { get; set; }
+        public string Description { get; set; }
+
+        public List<FileItemDto> File { get; set; }
+    }
+
+    public class UserSocialMediaDto
+    {
+        public int Id { get; set; }
+        public string Link { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string File { get; set; }
+
+    }
+
+    public class SocialMediaInputModel
+    {
+        public int Id { get; set; }
+        public string Link { get; set; }
+        public string Title { get; set; }
+
+        public List<IFormFile> File { get; set; }
+        public bool IsActive { get; set; }
+        public string IsActiveTitle { get; set; }
+        public string Description { get; set; }
+
+    }
+}
