e757a73d252117912f22795eb296bb5d14bee590
SinaRiyahi <sina.riyahi76@gmail.com>
2024-04-14 23:34:21 +0330

    Create Class ArticleCategoryDto

---
+++ Infrastructure/Models/Blog/ArticleCategoryDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Blog
+{
+    public class ArticleCategoryDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsActive { get; set; }
+        public string IsActiveTitle { get; set; }
+        public int? SortOrder { get; set; }
+    }
+}
