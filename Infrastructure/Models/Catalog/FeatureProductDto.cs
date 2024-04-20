afb464765bcf5b1cf380ad8fb4b49fe7b6279781
SinaRiyahi <sina.riyahi76@gmail.com>
2024-04-20 23:48:35 +0330

    Create class FeatureProductDto

---
+++ Infrastructure/Models/Catalog/FeatureProductDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Catalog
+{
+    public class FeatureProductDto
+    {
+        public string Key { get; set; }
+
+        public string FeatureValue { get; set; }
+        public string BrandName { get; set; }
+        public string ProductName { get; set; }
+        public string ProductEnName { get; set; }
+
+        public List<FeatureProductDto> Brands { get; set; } = new List<FeatureProductDto>();
+    }
+}
