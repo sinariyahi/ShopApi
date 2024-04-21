d62b3b997e1f147820df9ca6f276ba3abfca461e
SinaRiyahi <sina.riyahi76@gmail.com>
2024-04-21 23:51:44 +0330

    Create Class KoponDto

---
+++ Infrastructure/Models/Catalog/KoponDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Catalog
+{
+    public class KoponDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsActive { get; set; }
+        public string IsActiveTitle { get; set; }
+        public string ToDate { get; set; }
+        public string FromDate { get; set; }
+        public int Percent { get; set; }
+        public string Remark { get; set; }
+        public string Code { get; set; }
+
+    }
+
+    public class UserKoponDto
+    {
+        public int Percent { get; set; }
+        public string Code { get; set; }
+
+    }
+
+}
