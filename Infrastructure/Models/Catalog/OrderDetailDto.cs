1b3dd2f3026322c412aaeb4809ca2869b9376302
SinaRiyahi <sina.riyahi76@gmail.com>
2024-04-22 23:38:08 +0330

    Create Class OrderDetailDto

---
+++ Infrastructure/Models/Catalog/OrderDetailDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Catalog
+{
+    public class OrderDetailDto
+    {
+        public string EnTitle { get; set; }
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public Guid OrderId { get; set; }
+        public long ItemCount { get; set; }
+        public long Price { get; set; }
+        public long DiscountedPrice { get; set; }
+        public string BrandName { get; set; }
+        public string ProductName { get; set; }
+        public string CoverFile { get; set; }
+    }
+
+    public class UserOrderDetailDto
+    {
+        public int ProductId { get; set; }
+        public long Price { get; set; }
+        public long ItemCount { get; set; }
+    }
+
+
+}
