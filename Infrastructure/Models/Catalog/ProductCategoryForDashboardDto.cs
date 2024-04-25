3029cc29c55a7705880e6438c53002c3b85d1744
SinaRiyahi <sina.riyahi76@gmail.com>
2024-04-25 23:54:00 +0330

    Class Create ProductCategoryForDashboardDto

---
+++ Infrastructure/Models/Catalog/ProductCategoryForDashboardDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Catalog
+{
+    public class ProductCategoryForDashboardDto
+    {
+        public int ProductCategoryCount { get; set; }
+        public List<ProductCategoryForDashboardItemDto> Items { get; set; } = new List<ProductCategoryForDashboardItemDto>();
+    }
+
+    public class ProductCategoryForDashboardItemDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Value { get; set; }
+    }
+}
