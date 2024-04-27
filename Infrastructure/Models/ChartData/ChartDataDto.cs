392d6a46067950e54fff8fb0f4c6806fc22a1d2c
SinaRiyahi <sina.riyahi76@gmail.com>
2024-04-27 23:39:49 +0330

    Create Class ChartDataDto

---
+++ Infrastructure/Models/ChartData/ChartDataDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.ChartData
+{
+    public class ChartDataDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public double Value { get; set; }
+        public string Category { get; set; }
+    }
+
+    public class ChartColumnInfo
+    {
+        public string Title { get; set; }
+        public string ColumnName { get; set; }
+    }
+}
