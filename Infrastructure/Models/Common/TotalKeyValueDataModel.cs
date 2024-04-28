653815e95e50d816896d1b09c378757d57063f4f
SinaRiyahi <sina.riyahi76@gmail.com>
2024-04-28 23:52:14 +0330

    Create Class TotalKeyValueDataModel

---
+++ Infrastructure/Models/Common/TotalKeyValueDataModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Common
+{
+    public class TotalKeyValueDataModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Code { get; set; }
+        public bool IsOrganizationUnit { get; set; } = false;
+    }
+}
