18f252f6a488db869230fcfd0dcbf47a9715c447
SinaRiyahi <sina.riyahi76@gmail.com>
2024-04-28 23:49:44 +0330

    Create Class MailMessageDto

---
+++ Infrastructure/Models/Common/MailMessageDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Common
+{
+    public class MailMessageDto
+    {
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public List<string> CC { get; set; } = new List<string>();
+    }
+}
