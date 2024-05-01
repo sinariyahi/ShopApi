4b5e749c1a5541073e47d59533300b99c1b3f0da
SinaRiyahi <sina.riyahi76@gmail.com>
2024-05-01 23:52:01 +0330

    Create Class DashboardDto

---
+++ Infrastructure/Models/Dashboard/DashboardDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Dashboard
+{
+    public class DashboardDto
+    {
+        public string Icon { get; set; }
+        public string Title { get; set; }
+        public int Total { get; set; }
+        public string Color { get; set; }
+        public string Path { get; set; }
+
+    }
+}
