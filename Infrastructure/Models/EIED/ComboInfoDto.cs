7ad1992410a712ff7ebe259175052f6bca74f28b
SinaRiyahi <sina.riyahi76@gmail.com>
2024-05-02 23:38:30 +0330

    Create Class ComboInfoDto

---
+++ Infrastructure/Models/EIED/ComboInfoDto.cs
@@ -0,0 +1,47 @@
+﻿using Infrastructure.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.EIED
+{
+    public class ComboInfoDto
+    {
+        public List<ComboItemDto> OrganizationUnits { get; set; }
+        public List<ComboItemDto> ResponsiblePersonDataCollections { get; set; }
+        public List<ComboItemDto> Discpilines { get; set; }
+        public List<ComboItemDto> OrganizationLevel { get; set; }
+        public List<ComboItemDto> Projects { get; set; }
+        public List<ComboItemDto> Roles { get; set; }
+        public List<EIEDEnums> Years { get; set; }
+        public List<EIEDEnums> Months { get; set; }
+        public List<EIEDEnums> Operators { get; set; }
+        public List<EIEDEnums> TimeDurations { get; set; }
+        public List<EIEDEnums> UserTypes { get; set; }
+        public List<EIEDEnums> CalcTypes { get; set; }
+        public ApplicationConfigDto ApplicationConfig { get; set; }
+        public List<EIEDEnums> EquipmentTypes { get; set; }
+        public List<EIEDEnums> PersonnelPositions { get; set; }
+        public List<ComboItemDto> CompanyActicityTypes { get; set; }
+        public List<EIEDEnums> UnitTypes { get; set; }
+        public List<EIEDEnums> ControlTypes { get; set; }
+    }
+
+    public class ComboItemDto
+    {
+        public string EnTitle { get; set; }
+        public object Value { get; set; }
+        public string Text { get; set; }
+        public string Label { get; set; }
+        public object ParentId { get; set; }
+        public string ParentTitle { get; set; }
+    }
+
+    public class IndexTemplateComboItemDto : ComboItemDto
+    {
+        public List<EIEDEnums> ShowTypes { get; set; }
+        public ShowType DefaultShowType { get; set; }
+    }
+}
---
+++ Infrastructure/Models/EIED/EIEDEnums.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.EIED
+{
+    public class EIEDEnums
+    {
+        public int Value { get; set; }
+        public string Text { get; set; }
+    }
+    public class EIEDListEnums
+    {
+        public List<EIEDEnums> DashboardLevels { get; set; }
+        public List<EIEDEnums> MeasurementPeriods { get; set; }
+        public List<EIEDEnums> ShowTrends { get; set; }
+        public List<EIEDEnums> ShowTypes { get; set; }
+        public List<EIEDEnums> UnitTypes { get; set; }
+        public List<EIEDEnums> IndexTypes { get; set; }
+        public List<EIEDEnums> ActionTypes { get; set; }
+
+    }
+
+}
