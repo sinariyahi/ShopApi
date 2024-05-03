91bad107cd026820bef7f69eead97e8357d06068
SinaRiyahi <sina.riyahi76@gmail.com>
2024-05-03 23:48:24 +0330

    Create Class KeyValueDto

---
+++ Infrastructure/Models/Shop/KeyValueDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Shop
+{
+    public class KeyValueDto
+    {
+        public int Id { get; set; }
+        public int Value { get; set; }
+        public string Label { get; set; }
+        public string Text { get; set; }
+
+        public string Title { get; set; }
+    }
+
+    public class ParishItemDto
+    {
+        public int Id { get; set; }
+        public string ParishName { get; set; }
+        public int Value { get; set; }
+        public string Label { get; set; }
+        public string Text { get; set; }
+        public string Title { get; set; }
+
+    }
+}
