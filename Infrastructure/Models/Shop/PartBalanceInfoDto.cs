c34cfd6927406d71deae4aae694a7706bc310918
SinaRiyahi <sina.riyahi76@gmail.com>
2024-05-03 23:49:19 +0330

    Create Class PartBalanceInfoDto

---
+++ Infrastructure/Models/Shop/PartBalanceInfoDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Shop
+{
+    public class PartBalanceInfoDto
+    {
+        public string PartNo { get; set; }
+        public int? LegalAvail { get; set; }
+        public long? CustomerPrice { get; set; }
+    }
+}
