9f899533b1729c3004da8eb568b67ffc9dd4fd34
SinaRiyahi <sina.riyahi76@gmail.com>
2024-05-04 23:46:00 +0330

    Create Class VideoCategoryDto

---
+++ Infrastructure/Models/Media/VideoCategoryDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Media
+{
+    public class VideoCategoryDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsActive { get; set; }
+        public string IsActiveTitle { get; set; }
+        public int? SortOrder { get; set; }
+        public string Remark { get; set; }
+    }
+
+
+}
