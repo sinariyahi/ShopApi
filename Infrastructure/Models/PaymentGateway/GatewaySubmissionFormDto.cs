7301945c9dc423b7b9e148cee4bb211b97810d0f
SinaRiyahi <sina.riyahi76@gmail.com>
2024-05-07 23:22:40 +0330

    Create Class GatewaySubmissionFormDto

---
+++ Infrastructure/Models/PaymentGateway/GatewaySubmissionFormDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.PaymentGateway
+{
+    public class GatewaySubmissionFormDto
+    {
+        public string RefId { get; set; }
+    }
+
+    public class SalePaymentRequestModel
+    {
+        public long OrderId { get; set; }
+        public long Amount { get; set; }
+        public string CustomerMobileNumber { get; set; }
+    }
+
+    public class GatewayResponseDto
+    {
+        public string ResCode { get; set; }
+        public long SaleReferenceId { get; set; }
+        public long SaleOrderId { get; set; }
+        public long OrderId { get; set; }
+    }
+}
