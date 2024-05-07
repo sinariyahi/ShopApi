9f2abeffa95f10fe68bb4522d2004ba005f25409
SinaRiyahi <sina.riyahi76@gmail.com>
2024-05-07 23:24:38 +0330

    Create Class PaymentConfigs

---
+++ Infrastructure/Models/PaymentGateway/PaymentConfigs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.PaymentGateway
+{
+    public class PaymentConfigs
+    {
+        public long TerminalId { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public string CallBackUrl { get; set; }
+        public string PayerId { get; set; }
+        public string AppGatewayResult { get; set; }
+        public string PaymentPageUrl { get; set; }
+    }
+}
