85c8bd03bdd0f50b9015b401a9ec97deeee5efa3
SinaRiyahi <sina.riyahi76@gmail.com>
2024-05-16 23:56:16 +0330

    Create Class ContactFormDto

---
+++ Infrastructure/Models/Support/ContactFormDto.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Support
+{
+    public class ContactFormDto
+    {
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Remark { get; set; }
+        public string Subject { get; set; }
+        public bool IsVisited { get; set; }
+        public string CreateDate { get; set; }
+        public List<FileItemDto> File { get; set; }
+    }
+
+    public class UserContactFormDto
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Remark { get; set; }
+        public string Subject { get; set; }
+
+
+    }
+
+    public class ContactFormInputModel
+    {
+        public string Captcha { get; set; }
+        public string CaptchaKey { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Remark { get; set; }
+        public string Subject { get; set; }
+
+        public List<IFormFile> File { get; set; }
+
+
+    }
+}
