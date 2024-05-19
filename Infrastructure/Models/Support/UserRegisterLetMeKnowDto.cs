4b0d4a6f0474de60a401e80659fed56f2b44c48f
SinaRiyahi <sina.riyahi76@gmail.com>
2024-05-19 23:49:27 +0330

    Create Class UserRegisterLetMeKnowDtoDto

---
+++ Infrastructure/Models/Support/UserRegisterLetMeKnowDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Support
+{
+    public class UserRegisterLetMeKnowDtoDto
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string ProductName { get; set; }
+        public string CategoryName { get; set; }
+
+        public string Mobile { get; set; }
+        public string CreateDate { get; set; }
+
+    }
+
+
+    public class UserLetMeKnowDto
+    {
+        public int ProductId { get; set; }
+
+        public string Mobile { get; set; }
+        public string Captcha { get; set; }
+        public string CaptchaKey { get; set; }
+    }
+
+}
