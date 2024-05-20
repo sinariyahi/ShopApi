86645fd20a743cb8240f130d89a0d8ef3f0a1693
SinaRiyahi <sina.riyahi76@gmail.com>
2024-05-20 23:52:18 +0330

    Create Class ChangePasswordInputModel

---
+++ Infrastructure/Models/User/ChangePasswordInputModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.User
+{
+    public class ChangePasswordInputModel
+    {
+        public string Password { get; set; }
+        public string OldPassword { get; set; }
+        public Guid? UserId { get; set; }
+    }
+
+    public class ChangeEmailSignatureInputModel
+    {
+        public string EmailSignuature { get; set; }
+        public Guid? UserId { get; set; }
+    }
+}
