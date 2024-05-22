f0dde08b044ba97caf5f78c4a63fc04b247c0efd
SinaRiyahi <sina.riyahi76@gmail.com>
2024-05-22 23:30:36 +0330

    Class FavoriteProduct

--- Domain/Entities/Catalog/FavoriteProduct.cs
+++ Domain/Entities/Catalog/FavoriteProduct.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain.Entities.Security;
 
 namespace Domain.Entities.Catalog
 {
@@ -15,9 +16,9 @@ namespace Domain.Entities.Catalog
         public Guid Id { get; set; }
 
         public Guid UserId { get; set; }
-       // public virtual User User { get; set; }
+        public virtual User User { get; set; }
         public int ProductId { get; set; }
-     //   public virtual Product Product { get; set; }
+        public virtual Product Product { get; set; }
         public DateTime CreateDate { get; set; }
 
     }
