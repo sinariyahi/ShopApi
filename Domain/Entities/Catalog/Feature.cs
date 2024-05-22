75365cd76fbd3af8607bbb89be149682b1313568
SinaRiyahi <sina.riyahi76@gmail.com>
2024-05-22 23:31:22 +0330

    Class Feature

--- Domain/Entities/Catalog/Feature.cs
+++ Domain/Entities/Catalog/Feature.cs
@@ -5,6 +5,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Infrastructure.Common;
 
 namespace Domain.Entities.Catalog
 {
@@ -16,17 +17,17 @@ namespace Domain.Entities.Catalog
         public string Title { get; set; }
         [MaxLength(256)]
         public string Remark { get; set; }
-     //   public UnitType UnitType { get; set; }
-      //  public ControlType ControlType { get; set; }
+        public UnitType UnitType { get; set; }
+        public ControlType ControlType { get; set; }
         public DateTime CreateDate { get; set; }
         public int? FeatureCategoryId { get; set; }
-     //   public virtual FeatureCategory FeatureCategory { get; set; }
+        public virtual FeatureCategory FeatureCategory { get; set; }
         public int? Max { get; set; }
         public int? Min { get; set; }
         public string Option { get; set; }
         public bool ShowInFilter { get; set; } = false;
 
         public int? SymbolId { get; set; }
-     //   public virtual Symbol Symbol { get; set; }
+        public virtual Symbol Symbol { get; set; }
     }
 }
