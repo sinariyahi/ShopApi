90063d5e71dca0613d56b41105e1518c82a99595
SinaRiyahi <sina.riyahi76@gmail.com>
2024-05-26 22:31:27 +0330

    Class UserSms

--- Domain/Entities/Security/UserSms.cs
+++ Domain/Entities/Security/UserSms.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.InteropServices.JavaScript;
 using System.Text;
 using System.Threading.Tasks;
+using Infrastructure.Common;
 
 namespace Domain.Entities.Security
 {
@@ -18,7 +19,7 @@ namespace Domain.Entities.Security
         public Guid? UserId { get; set; }
         public virtual User User { get; set; }
         public string Code { get; set; }
-     //   public SmsType Type { get; set; }
+        public SmsType Type { get; set; }
         public DateTime ActionTime { get; set; }
         public bool Status { get; set; }
         public string PhoneNumber { get; set; }
