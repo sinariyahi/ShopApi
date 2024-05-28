9abab88ba9a0838cb930e04f80ef959378e8bc08
SinaRiyahi <sina.riyahi76@gmail.com>
2024-05-28 23:39:31 +0330

    Class User

--- Domain/Entities/Security/User.cs
+++ Domain/Entities/Security/User.cs
@@ -6,6 +6,8 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Infrastructure.Common;
+using Domain.Entities.Support;
 
 namespace Domain.Entities.Security
 {
@@ -33,8 +35,8 @@ namespace Domain.Entities.Security
         public bool IsConfirmPhoneNumber { get; set; } = false;
         [MaxLength(32)]
         public string PhoneNumber { get; set; }
-        //public Sex? Sex { get; set; }
-        //public UserType? UserType { get; set; }
+        public Sex? Sex { get; set; }
+        public UserType? UserType { get; set; }
         public Guid? ConfirmationCode { get; set; }
         public DateTime? ConfirmationDate { get; set; }
 
@@ -46,23 +48,23 @@ namespace Domain.Entities.Security
 
         public string EmailSignature { get; set; }
 
-        //public ICollection<UserRole> UserRoles { get; set; }
-        //public ICollection<UserProject> UserProjects { get; set; }
-        //public ICollection<UserDiscipline> UserDisciplines { get; set; }
-        //public ICollection<UserOrganizationUnit> UserOrganizationUnits { get; set; }
-        //public ICollection<UserAnswer> UserAnswers { get; set; }
+        public ICollection<UserRole> UserRoles { get; set; }
+        public ICollection<UserProject> UserProjects { get; set; }
+        public ICollection<UserDiscipline> UserDisciplines { get; set; }
+        public ICollection<UserOrganizationUnit> UserOrganizationUnits { get; set; }
+        public ICollection<UserAnswer> UserAnswers { get; set; }
         public ICollection<UserCartable> UserCartables { get; set; }
         public ICollection<UserPayment> UserPayments { get; set; }
 
 
         public User()
         {
-            //UserProjects = new HashSet<UserProject>();
-            //UserDisciplines = new HashSet<UserDiscipline>();
-            //UserOrganizationUnits = new HashSet<UserOrganizationUnit>();
-            //UserAnswers = new HashSet<UserAnswer>();
-            //UserCartables = new HashSet<UserCartable>();
-            //UserPayments = new HashSet<UserPayment>();
+            UserProjects = new HashSet<UserProject>();
+            UserDisciplines = new HashSet<UserDiscipline>();
+            UserOrganizationUnits = new HashSet<UserOrganizationUnit>();
+            UserAnswers = new HashSet<UserAnswer>();
+            UserCartables = new HashSet<UserCartable>();
+            UserPayments = new HashSet<UserPayment>();
 
         }
     }
