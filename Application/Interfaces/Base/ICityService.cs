e25573d708f1536f09bcf7c565112572b420e161
SinaRiyahi <sina.riyahi76@gmail.com>
2024-06-02 23:31:31 +0330

    interface ICityService

--- Application/Interfaces/Base/ICityService.cs
+++ Application/Interfaces/Base/ICityService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,13 @@ using System.Threading.Tasks;
 
 namespace Application.Interfaces.Base
 {
-    internal interface ICityService
+    public interface ICityService
     {
+
+        Task<ShopActionResult<List<CityDto>>> GetList(GridQueryModel model = null);
+        Task<ShopActionResult<int>> Add(CityInputModel model);
+        Task<ShopActionResult<int>> Update(CityInputModel model);
+        Task<ShopActionResult<int>> Delete(int id);
+        Task<ShopActionResult<CityDto>> GetById(int id);
     }
 }
