7ee44d988c5d731a2eb8e3c4891b7cd679f795c5
SinaRiyahi <sina.riyahi76@gmail.com>
2024-06-05 23:22:59 +0330

    interface ICurrencyService

--- Application/Interfaces/Base/ICurrencyService.cs
+++ Application/Interfaces/Base/ICurrencyService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Base;
+using Infrastructure.Models.EIED;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,11 @@ namespace Application.Interfaces.Base
 {
     public interface ICurrencyService
     {
+        Task<ShopActionResult<List<CurrencyDto>>> GetList(GridQueryModel model = null);
+        Task<List<ComboItemDto>> GetForCombo();
+        Task<ShopActionResult<int>> Add(CurrencyDto model);
+        Task<ShopActionResult<int>> Update(CurrencyDto model);
+        Task<ShopActionResult<int>> Delete(int id);
+        Task<ShopActionResult<CurrencyDto>> GetById(int id);
     }
 }
