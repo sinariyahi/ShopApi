2256dbd8792cdae93adb4c084bd69a4ff93ba586
SinaRiyahi <sina.riyahi76@gmail.com>
2024-06-05 23:24:35 +0330

    interface IEmailTemplateService

--- Application/Interfaces/Base/IEmailTemplateService.cs
+++ Application/Interfaces/Base/IEmailTemplateService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Base;
+using Infrastructure.Models.EIED;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,14 @@ namespace Application.Interfaces.Base
 {
     public interface IEmailTemplateService
     {
+        Task<ShopActionResult<List<EmailTemplateDto>>> GetList(GridQueryModel model = null);
+        Task<ShopActionResult<int>> Add(EmailTemplateDto model);
+        Task<ShopActionResult<int>> Update(EmailTemplateDto model);
+        Task<ShopActionResult<int>> Delete(int id);
+        Task<ShopActionResult<EmailTemplateDto>> GetById(int id);
+        Task<List<ComboItemDto>> GetTemplateTypesForCombo();
+        Task<string> GenerateEmailTemplate(EmailTemplateType emailTemplateType, EmailSenderInfoDto emailSenderInfo, params string[] values);
+        Task<string> GenerateEmailTemplateWithMessageBody(EmailSenderInfoDto emailSenderInfo, string messageBody);
+        Task<ShopActionResult<bool>> ResetTemplate(int id);
     }
 }
