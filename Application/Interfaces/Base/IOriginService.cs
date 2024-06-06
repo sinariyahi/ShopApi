b444e44c3aa94ee702852459fcba2c2f8abc6414
SinaRiyahi <sina.riyahi76@gmail.com>
2024-06-06 23:52:07 +0330

    interface IOriginService

--- Application/Interfaces/Base/IOriginService.cs
+++ Application/Interfaces/Base/IOriginService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,12 @@ using System.Threading.Tasks;
 
 namespace Application.Interfaces.Base
 {
-    internal interface IOriginService
+    public interface IOriginService
     {
+        Task<ShopActionResult<List<OriginDto>>> GetList(GridQueryModel model = null);
+        Task<ShopActionResult<int>> Add(OriginDto model);
+        Task<ShopActionResult<int>> Update(OriginDto model);
+        Task<ShopActionResult<int>> Delete(int id);
+        Task<ShopActionResult<OriginDto>> GetById(int id);
     }
 }
