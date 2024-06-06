bbe691cbaca0465f663dd9c80699242c90bc84d4
SinaRiyahi <sina.riyahi76@gmail.com>
2024-06-06 23:41:53 +0330

    interface INewsLetterService

--- Application/Interfaces/Base/INewsLetterService.cs
+++ Application/Interfaces/Base/INewsLetterService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,14 @@ using System.Threading.Tasks;
 
 namespace Application.Interfaces.Base
 {
-    internal interface INewsLetterService
+    public interface INewsLetterService
     {
+        Task<ShopActionResult<List<NewsLetterDto>>> GetList(GridQueryModel model = null);
+        Task<ShopActionResult<int>> Add(NewsLetterDto model);
+        Task<ShopActionResult<int>> Update(NewsLetterDto model);
+        Task<ShopActionResult<int>> Delete(int id);
+        Task<ShopActionResult<NewsLetterDto>> GetById(int id);
+        Task<ShopActionResult<List<UserRegisterNewsLetterDto>>> GetUserRegisterNewsLetter(GridQueryModel model = null);
+        Task<ShopActionResult<int>> Register(UserNewsLetterDto model, Guid? userId);
     }
 }
