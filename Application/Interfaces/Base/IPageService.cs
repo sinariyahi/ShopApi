d953a2fb17cb587966e985239804d425f32c6e16
SinaRiyahi <sina.riyahi76@gmail.com>
2024-06-07 22:52:47 +0330

    interface IPageService

--- Application/Interfaces/Base/IPageService.cs
+++ Application/Interfaces/Base/IPageService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,16 @@ using System.Threading.Tasks;
 
 namespace Application.Interfaces.Base
 {
-    internal interface IPageService
+    public interface IPageService
     {
+        Task<ShopActionResult<List<string>>> GetAllLink();
+
+        Task<ShopActionResult<List<PageDto>>> GetList(GridQueryModel model = null);
+        Task<ShopActionResult<int>> Add(PageInputModel model);
+        Task<ShopActionResult<int>> Update(PageInputModel model);
+        Task<ShopActionResult<int>> Delete(Guid id);
+        Task<ShopActionResult<PageDto>> GetById(Guid id);
+        Task<ShopActionResult<PageDto>> GetByTitle(string title);
+        Task<ShopActionResult<List<UserPageDto>>> GetListUserPages();
     }
 }
