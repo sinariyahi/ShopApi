d1b62974437ca93e1524ae15a48091a9290cf734
SinaRiyahi <sina.riyahi76@gmail.com>
2024-06-08 22:13:32 +0330

    interface IProjectService

--- Application/Interfaces/Base/IProjectService.cs
+++ Application/Interfaces/Base/IProjectService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.EIED;
+using Infrastructure.Models.Project;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,13 @@ using System.Threading.Tasks;
 
 namespace Application.Interfaces.Base
 {
-    internal interface IProjectService
+    public interface IProjectService
     {
+        Task<ShopActionResult<List<ComboItemDto>>> GetForCombo();
+        Task<ShopActionResult<List<ProjectDto>>> GetList(GridQueryModel model = null);
+        Task<ShopActionResult<int>> Add(ProjectDto model);
+        Task<ShopActionResult<int>> Update(ProjectDto model);
+        Task<ShopActionResult<int>> Delete(int id);
+        Task<ShopActionResult<ProjectDto>> GetById(int id);
     }
 }
