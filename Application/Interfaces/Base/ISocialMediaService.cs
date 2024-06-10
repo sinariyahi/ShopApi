1d147be73f7d047ba2166afa5a44a59658835a1b
SinaRiyahi <sina.riyahi76@gmail.com>
2024-06-10 22:57:31 +0330

    interface ISocialMediaService

--- Application/Interfaces/Base/ISocialMediaService.cs
+++ Application/Interfaces/Base/ISocialMediaService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,11 @@ namespace Application.Interfaces.Base
 {
     public interface ISocialMediaService
     {
+        Task<ShopActionResult<List<SocialMediaDto>>> GetList(GridQueryModel model = null);
+        Task<ShopActionResult<List<UserSocialMediaDto>>> GetUserList(int count = 8);
+        Task<ShopActionResult<int>> Add(SocialMediaInputModel model);
+        Task<ShopActionResult<int>> Update(SocialMediaInputModel model);
+        Task<ShopActionResult<int>> Delete(int id);
+        Task<ShopActionResult<SocialMediaDto>> GetById(int id);
     }
 }
