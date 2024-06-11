f63b873324a31719c5e3f8e2bbfa1609bb1ac044
SinaRiyahi <sina.riyahi76@gmail.com>
2024-06-11 23:43:07 +0330

    interface IArticleCategoryService

--- Application/Interfaces/Blog/IArticleCategoryService.cs
+++ Application/Interfaces/Blog/IArticleCategoryService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Blog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,11 @@ namespace Application.Interfaces.Blog
 {
     public interface IArticleCategoryService
     {
+        Task<ShopActionResult<List<ArticleCategoryDto>>> GetList(GridQueryModel model = null);
+        Task<ShopActionResult<int>> Add(ArticleCategoryDto model);
+        Task<ShopActionResult<int>> Update(ArticleCategoryDto model);
+        Task<ShopActionResult<int>> Delete(int id);
+        Task<ShopActionResult<ArticleCategoryDto>> GetById(int id);
+        Task<ShopActionResult<List<ArticleCategoryDto>>> GetUserList();
     }
 }
