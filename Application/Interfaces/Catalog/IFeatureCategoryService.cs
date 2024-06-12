e126bb88b61f6274668228cc74891892f9242b05
SinaRiyahi <sina.riyahi76@gmail.com>
2024-06-12 23:23:59 +0330

    interface IFeatureCategoryService

--- Application/Interfaces/Catalog/IFeatureCategoryService.cs
+++ Application/Interfaces/Catalog/IFeatureCategoryService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Catalog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,10 @@ namespace Application.Interfaces.Catalog
 {
     public interface IFeatureCategoryService
     {
+        Task<ShopActionResult<List<FeatureCategoryDto>>> GetList(GridQueryModel model = null);
+        Task<ShopActionResult<int>> Add(FeatureCategoryDto model);
+        Task<ShopActionResult<int>> Update(FeatureCategoryDto model);
+        Task<ShopActionResult<int>> Delete(int id);
+        Task<ShopActionResult<FeatureCategoryDto>> GetById(int id);
     }
 }
