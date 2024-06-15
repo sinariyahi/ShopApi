2af2cea301c292e26b882f32a7956075ef48d7cb
SinaRiyahi <sina.riyahi76@gmail.com>
2024-06-15 23:45:59 +0330

    interface IKoponService

--- Application/Interfaces/Catalog/IKoponService.cs
+++ Application/Interfaces/Catalog/IKoponService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Catalog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,11 @@ namespace Application.Interfaces.Catalog
 {
     public interface IKoponService
     {
+        Task<ShopActionResult<List<KoponDto>>> GetList(GridQueryModel model = null);
+        Task<ShopActionResult<int>> Add(KoponDto model);
+        Task<ShopActionResult<int>> Update(KoponDto model);
+        Task<ShopActionResult<int>> Delete(int id);
+        Task<ShopActionResult<KoponDto>> GetById(int id);
+        Task<ShopActionResult<UserKoponDto>> GetByCode(string code);
     }
 }
