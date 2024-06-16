df2e308877079089211a09720abb71b1306dd834
SinaRiyahi <sina.riyahi76@gmail.com>
2024-06-16 23:48:33 +0330

    interface ISymbolService

---
+++ Application/Interfaces/Catalog/ISymbolService.cs
@@ -0,0 +1,21 @@
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Base;
+using Infrastructure.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Catalog
+{
+    internal interface ISymbolService
+    {
+        Task<ShopActionResult<List<SymbolDto>>> GetList(GridQueryModel model = null);
+        Task<ShopActionResult<int>> Add(SymbolDto model);
+        Task<ShopActionResult<int>> Update(SymbolDto model);
+        Task<ShopActionResult<int>> Delete(int id);
+        Task<ShopActionResult<SymbolDto>> GetById(int id);
+        Task<ShopActionResult<List<TreeDto>>> GetTree(int? parentId);
+    }
+}
---
+++ Infrastructure/Models/Base/SymbolDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models.Base
+{
+    public class SymbolDto
+    {
+        public int Id { get; set; }
+        public int? ParentId { get; set; }
+        public string Title { get; set; }
+
+        public bool IsActive { get; set; }
+        public string IsActiveTitle { get; set; }
+
+    }
+}
