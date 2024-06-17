dd5fe554eee724dcbb26fdb4e73ac50975934b31
SinaRiyahi <sina.riyahi76@gmail.com>
2024-06-17 23:53:59 +0330

    Class VendorEmployeePositionDto

--- Infrastructure/Models/Base/VendorEmployeePositionDto.cs
+++ Infrastructure/Models/Base/VendorEmployeePositionDto.cs
@@ -8,5 +8,11 @@ namespace Infrastructure.Models.Base
 {
     public class VendorEmployeePositionDto
     {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public bool IsActive { get; set; }
+        public string IsActiveTitle { get; set; }
     }
 }
