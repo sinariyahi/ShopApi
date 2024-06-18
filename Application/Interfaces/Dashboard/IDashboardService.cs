73e5d0338f5ae28db6254215ec993ca5afb765f3
SinaRiyahi <sina.riyahi76@gmail.com>
2024-06-18 23:25:17 +0330

    interface IDashboardService

--- Application/Interfaces/Dashboard/IDashboardService.cs
+++ Application/Interfaces/Dashboard/IDashboardService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Dashboard;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,6 @@ namespace Application.Interfaces.Dashboard
 {
     public interface IDashboardService
     {
+        Task<ShopActionResult<List<DashboardDto>>> GetCountData();
     }
 }
