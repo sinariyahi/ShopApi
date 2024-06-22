b67715389b48dac46613793446fd26eb3a699af9
SinaRiyahi <sina.riyahi76@gmail.com>
2024-06-22 23:57:25 +0330

    Create interface IBannerService

--- Application/Interfaces/Media/IBannerService.cs
+++ Application/Interfaces/Media/IBannerService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Media;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,11 @@ namespace Application.Interfaces.Media
 {
     public interface IBannerService
     {
+        Task<ShopActionResult<List<BannerDto>>> GetList(GridQueryModel model = null);
+        Task<ShopActionResult<int>> Add(BannerDto model);
+        Task<ShopActionResult<int>> Update(BannerDto model);
+        Task<ShopActionResult<int>> Delete(int id);
+        Task<ShopActionResult<BannerDto>> GetById(int id);
+        Task<ShopActionResult<UserBannerDto>> GetUserList(PositionPlace positionPlace);
     }
 }
