f896678e65a3179fb06e302bcf18bf11f167af80
SinaRiyahi <sina.riyahi76@gmail.com>
2024-06-23 23:54:31 +0330

    interface IVideoCategoryService

--- Application/Interfaces/Media/IVideoCategoryService.cs
+++ Application/Interfaces/Media/IVideoCategoryService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Media;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,11 @@ namespace Application.Interfaces.Media
 {
     public interface IVideoCategoryService
     {
+        Task<ShopActionResult<List<VideoCategoryDto>>> GetList(GridQueryModel model = null);
+        Task<ShopActionResult<int>> Add(VideoCategoryDto model);
+        Task<ShopActionResult<int>> Update(VideoCategoryDto model);
+        Task<ShopActionResult<int>> Delete(int id);
+        Task<ShopActionResult<VideoCategoryDto>> GetById(int id);
+        Task<ShopActionResult<List<VideoCategoryDto>>> GetUserList();
     }
 }
