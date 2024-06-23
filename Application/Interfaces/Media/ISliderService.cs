4a22ec57e810f2e7fca7e82026b6cd2d5f780d93
SinaRiyahi <sina.riyahi76@gmail.com>
2024-06-23 23:53:03 +0330

    interface ISliderService

--- Application/Interfaces/Media/ISliderService.cs
+++ Application/Interfaces/Media/ISliderService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Media;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,10 @@ namespace Application.Interfaces.Media
 {
     public interface ISliderService
     {
+        Task<ShopActionResult<List<SliderDto>>> GetList(GridQueryModel model = null);
+        Task<ShopActionResult<int>> Add(SliderDto model);
+        Task<ShopActionResult<int>> Update(SliderDto model);
+        Task<ShopActionResult<int>> Delete(int id);
+        Task<ShopActionResult<SliderDto>> GetById(int id);
     }
 }
