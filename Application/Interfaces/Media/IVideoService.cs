9288e1ac7b44aff9c875d1db7865ddd12323b4bc
SinaRiyahi <sina.riyahi76@gmail.com>
2024-06-25 21:22:43 +0330

    interface IVideoService

--- Application/Interfaces/Media/IVideoService.cs
+++ Application/Interfaces/Media/IVideoService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Media;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,14 @@ namespace Application.Interfaces.Media
 {
     public interface IVideoService
     {
+        Task<ShopActionResult<List<VideoDto>>> GetList(GridQueryModel model = null);
+        Task<ShopActionResult<int>> Add(VideoDto model);
+        Task<ShopActionResult<int>> Update(VideoDto model);
+        Task<ShopActionResult<int>> Delete(int id);
+        Task<ShopActionResult<VideoDto>> GetById(int id);
+
+        Task<ShopActionResult<List<UserVideoDto>>> GetLastVideo();
+        Task<ShopActionResult<List<UserVideoDto>>> GetUserList(GridQueryModel model = null);
+        Task<ShopActionResult<List<int>>> GetAllId();
     }
 }
