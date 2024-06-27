649906a9eb02c41546754f734cd4d09dcae61578
SinaRiyahi <sina.riyahi76@gmail.com>
2024-06-27 23:47:50 +0330

    interface IOrderService

--- Application/Interfaces/Order/IOrderService.cs
+++ Application/Interfaces/Order/IOrderService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Catalog;
+using Infrastructure.Models.Payment;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,13 @@ namespace Application.Interfaces.Order
 {
     public interface IOrderService
     {
+        Task<ShopActionResult<List<OrderDto>>> GetList(OrderFilterDto model = null);
+        Task<ShopActionResult<UserPaymenstDto>> Add(UserOrderDto model, Guid userId);
+        Task<ShopActionResult<Guid>> Update(OrderInputDto model, Guid userId);
+        Task<ShopActionResult<Guid>> Delete(Guid id);
+        Task<ShopActionResult<OrderDto>> GetById(Guid id);
+        Task<ShopActionResult<List<OrderModel>>> GetDetailById(Guid userId);
+        Task<byte[]> GetListForExcel(GridQueryModel model = null, string fileName = null);
+        Task<ShopActionResult<Guid>> ReturnOrderByCustomer(ReturnOrderInputDto model, Guid userId);
     }
 }
