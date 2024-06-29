b10f7f6e3fbc398c681dc6e5d14459697a26acde
SinaRiyahi <sina.riyahi76@gmail.com>
2024-06-29 23:30:09 +0330

    interface IUserPaymentService

--- Application/Interfaces/Payment/IUserPaymentService.cs
+++ Application/Interfaces/Payment/IUserPaymentService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Payment;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,7 @@ namespace Application.Interfaces.Payment
 {
     public interface IUserPaymentService
     {
+        Task<ShopActionResult<List<UserPaymenstDto>>> GetUserAllPayments(UserPaymentFilterDto model, Guid userId);
+        Task<ShopActionResult<List<UserPaymenstModel>>> GetUserAllPaymentsForUser(Guid userId);
     }
 }
