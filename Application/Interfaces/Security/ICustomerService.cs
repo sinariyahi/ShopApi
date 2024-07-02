0b39b4b0597316fcfa548e809e77afee554c529c
SinaRiyahi <sina.riyahi76@gmail.com>
2024-07-02 23:52:07 +0330

    interface ICustomerService

--- Application/Interfaces/Security/ICustomerService.cs
+++ Application/Interfaces/Security/ICustomerService.cs
@@ -12,7 +12,14 @@ namespace Application.Interfaces.Security
 {
     public interface ICustomerService
     {
-
+        Task<ShopActionResult<List<CustomerDto>>> GetCustomers(GridQueryModel model = null);
+        Task<ShopActionResult<CustomerInputModel>> AddCustomer(CustomerInputModel model, bool isAdmin = true);
+        Task<ShopActionResult<CustomerInputModel>> UpdateCustomer(CustomerInputModel model, bool isAdmin = true);
+        Task<ShopActionResult<CustomerDto>> GetCustomerById(Guid userId);
+        Task<ShopActionResult<Guid>> UpdatePassword(ChangePasswordInputModel model, bool isCurrentUser = false);
+        Task<ShopActionResult<AuthenticateModel>> RegisterCustomer(RegisterCustomerInputModel model);
+        Task<ShopActionResult<CustomerInputModel>> UpdateCustomerByUser(CustomerInputModel model);
+        Task<ShopActionResult<CustomerInputModel>> UpdateCustomerInfoByUser(CustomerInputModel model);
 
     }
 }
