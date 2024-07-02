40763251f61cb12461121c4a353bd597ffeff654
SinaRiyahi <sina.riyahi76@gmail.com>
2024-07-02 23:51:37 +0330

    Create interface ICustomerService

---
+++ Application/Interfaces/Security/ICustomerService.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Authorization;
+using Infrastructure.Models.Customer;
+using Infrastructure.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.Security
+{
+    public interface ICustomerService
+    {
+
+
+    }
+}
