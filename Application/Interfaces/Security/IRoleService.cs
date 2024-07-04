a9aa494d28df43fe9e1fbfc34955b1c86d72204b
SinaRiyahi <sina.riyahi76@gmail.com>
2024-07-04 23:45:49 +0330

    interface IRoleService

--- Application/Interfaces/Security/IRoleService.cs
+++ Application/Interfaces/Security/IRoleService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.User;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,14 @@ namespace Application.Interfaces.Security
 {
     public interface IRoleService
     {
+        Task<ShopActionResult<List<RoleDto>>> GetRoles(GridQueryModel model = null);
+        Task<ShopActionResult<int>> Add(RoleDto model);
+        Task<ShopActionResult<int>> Update(RoleDto model);
+        Task<ShopActionResult<int>> Delete(int id);
+        Task<ShopActionResult<RoleDto>> GetById(int id);
+        Task<ShopActionResult<List<TreeDto>>> GetRoleTree(int? parentId);
+        Task<ShopActionResult<List<RoleGroupPermissionDto>>> GetRolePermissions(int roleId);
+        Task<ShopActionResult<int>> UpdateRolePermissions(int roleId, List<RolePermissionDto> permissions);
+        Task<ShopActionResult<List<TreeDto>>> GetOragnizationChartData(int? parentId = null);
     }
 }
