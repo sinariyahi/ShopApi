160cb1448752e057023a1305c2f6f1e8628847a1
SinaRiyahi <sina.riyahi76@gmail.com>
2024-07-04 23:57:06 +0330

    interface IUserService

--- Application/Interfaces/Security/IUserService.cs
+++ Application/Interfaces/Security/IUserService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.EIED;
+using Infrastructure.Models.User;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,15 @@ namespace Application.Interfaces.Security
 {
     public interface IUserService
     {
+        Task<ShopActionResult<List<UserDto>>> GetUsers(GridQueryModel model = null);
+        Task<ShopActionResult<UserInputModel>> AddUser(UserInputModel model, Guid userId);
+        Task<ShopActionResult<UserInputModel>> UpdateUser(UserInputModel model, Guid userId);
+        Task<ShopActionResult<Guid>> DeleteUser(Guid userid);
+        Task<ShopActionResult<UserDto>> GetUserById(Guid userid);
+        Task<ShopActionResult<string>> GetEmailSignuature(Guid userid);
+        Task<ShopActionResult<Guid>> UpdatePassword(ChangePasswordInputModel model, bool isCurrentUser = false);
+        Task<ShopActionResult<bool>> ChangeEmailSignature(ChangeEmailSignatureInputModel model);
+        Task<List<UserDto>> GetUsersInfoForCombo();
+        Task<List<ComboItemDto>> GetUsersForCombo();
     }
 }
