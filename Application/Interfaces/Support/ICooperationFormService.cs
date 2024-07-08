29d46bfd37a388b5d382c3c946ed1d344e66cf0a
SinaRiyahi <sina.riyahi76@gmail.com>
2024-07-08 23:48:35 +0330

    Create interface ICooperationFormService

--- Application/Interfaces/Support/ICooperationFormService.cs
+++ Application/Interfaces/Support/ICooperationFormService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Support;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,9 @@ namespace Application.Interfaces.Support
 {
     public interface ICooperationFormService
     {
+        Task<ShopActionResult<List<CooperationFormDto>>> GetList(GridQueryModel model = null);
+        Task<ShopActionResult<int>> Add(CooperationFormInputModel model);
+        Task<ShopActionResult<int>> Delete(int id);
+        Task<ShopActionResult<CooperationFormDto>> GetById(int id);
     }
 }
