2ffdc1dcdc882670bde5ed295b99c756838e37ef
SinaRiyahi <sina.riyahi76@gmail.com>
2024-07-08 23:52:53 +0330

    interface IUserOpinionService

--- Application/Interfaces/Support/IUserOpinionService.cs
+++ Application/Interfaces/Support/IUserOpinionService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,12 @@ namespace Application.Interfaces.Support
 {
     public interface IUserOpinionService
     {
+        Task<ShopActionResult<List<ListUserOpinionDto>>> GetAll(FilterUserOpinionDto model = null);
+        Task<ShopActionResult<UserOpinionDto>> GetById(Guid id);
+        Task<ShopActionResult<List<UserOpinionDto>>> GetByProductId(int ProductId);
+        Task<ShopActionResult<int>> Add(UserOpinionDto model);
+        Task<ShopActionResult<int>> Update(UserInputOpinionModel model);
+        Task<ShopActionResult<int>> Delete(Guid id);
+        Task<ShopActionResult<List<UserOpinionDto>>> GetByArticleId(int articleId);
     }
 }
