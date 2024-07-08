0c66ea452c7d82d16adcd3c4dd9a545b100a55ab
SinaRiyahi <sina.riyahi76@gmail.com>
2024-07-08 23:44:46 +0330

    interface IContactFormService

--- Application/Interfaces/Support/IContactFormService.cs
+++ Application/Interfaces/Support/IContactFormService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Infrastructure.Models.Support;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,9 @@ namespace Application.Interfaces.Support
 {
     public interface IContactFormService
     {
+        Task<ShopActionResult<List<ContactFormDto>>> GetList(GridQueryModel model = null);
+        Task<ShopActionResult<int>> Add(ContactFormInputModel model);
+        Task<ShopActionResult<int>> Delete(int id);
+        Task<ShopActionResult<ContactFormDto>> GetById(int id);
     }
 }
