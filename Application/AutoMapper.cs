52b808c7974b325e02620a80720830594879fd04
SinaRiyahi <sina.riyahi76@gmail.com>
2024-07-13 21:10:51 +0330

    class AutoMapper

--- Application/AutoMapper.cs
+++ Application/AutoMapper.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using AutoMapper;
+using Domain.Entities.Base;
+using Infrastructure.Models.Project;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Application
 {
-    public class AutoMapper
+    public class AutoMapper : Profile
     {
+        public AutoMapper()
+        {
+            CreateMap<Project, ProjectDto>().ReverseMap();
+        }
     }
 }
