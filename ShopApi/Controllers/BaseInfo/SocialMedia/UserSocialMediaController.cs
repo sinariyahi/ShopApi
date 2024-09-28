db8b92326611cea5db1d915fa860a0adae2cc178
SinaRiyahi <sina.riyahi76@gmail.com>
2024-09-28 22:51:05 +0330

    Class UserSocialMediaController

--- ShopApi/Controllers/BaseInfo/SocialMedia/UserSocialMediaController.cs
+++ ShopApi/Controllers/BaseInfo/SocialMedia/UserSocialMediaController.cs
@@ -1,6 +1,28 @@
-﻿namespace ShopApi.Controllers.BaseInfo.SocialMedia
+﻿using Application.Interfaces.Base;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApi.Controllers.BaseInfo.SocialMedia
 {
-    public class UserSocialMediaController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserSocialMediaController : ControllerBase
     {
+        private readonly ISocialMediaService _service;
+        public UserSocialMediaController(ISocialMediaService service)
+        {
+            _service = service;
+        }
+        /// <summary>
+        ///   برگرداندن  SocialMediaController 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("GetUserList/{count}")]
+        public async Task<IActionResult> GetUserList(int count)
+        {
+            var result = await _service.GetUserList(count);
+            return Ok(result);
+        }
+
+
     }
 }
