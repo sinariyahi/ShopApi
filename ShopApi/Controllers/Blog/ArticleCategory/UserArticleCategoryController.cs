dcb7f39971e382c83e5d21c62bf68698098274a6
SinaRiyahi <sina.riyahi76@gmail.com>
2024-09-30 23:53:23 +0330

    Class UserArticleCategoryController

--- ShopApi/Controllers/Blog/ArticleCategory/UserArticleCategoryController.cs
+++ ShopApi/Controllers/Blog/ArticleCategory/UserArticleCategoryController.cs
@@ -1,6 +1,48 @@
-﻿namespace ShopApi.Controllers.Blog.ArticleCategory
+﻿using Application.Interfaces.Blog;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApi.Controllers.Blog.ArticleCategory
 {
-    public class UserArticleCategoryController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserArticleCategoryController : ControllerBase
     {
+        private readonly IArticleCategoryService articleCategoryService;
+
+        public UserArticleCategoryController(IArticleCategoryService articleCategoryService)
+        {
+            this.articleCategoryService = articleCategoryService;
+        }
+
+
+        /// <summary>
+        ///   برگرداندن یک مقاله 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("GetById/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await articleCategoryService.GetById(id);
+            return Ok(result);
+        }
+
+
+        /// <summary>
+        ///   برگرداندن یک مقاله 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("GetUserList")]
+        public async Task<IActionResult> GetUserList()
+        {
+            var result = await articleCategoryService.GetUserList();
+            return Ok(result);
+        }
+
+
+
+
+
+
+
     }
 }
