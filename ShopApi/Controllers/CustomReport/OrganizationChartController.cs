5418ed9e4a160aeae2c13fde5158e90726d13241
SinaRiyahi <sina.riyahi76@gmail.com>
2024-10-21 00:30:29 +0330

    OrganizationChartController

--- ShopApi/Controllers/CustomReport/OrganizationChartController.cs
+++ ShopApi/Controllers/CustomReport/OrganizationChartController.cs
@@ -1,6 +1,21 @@
-﻿namespace ShopApi.Controllers.CustomReport
+﻿using Application.Interfaces.Security;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApi.Controllers.CustomReport
 {
-    public class OrganizationChartController
+    public class OrganizationChartController : BaseController
     {
+        private readonly IRoleService roleService;
+        public OrganizationChartController(IRoleService roleService)
+        {
+            this.roleService = roleService;
+        }
+
+        [HttpGet("ShowTree")]
+        public async Task<IActionResult> ShowTree()
+        {
+            var result = await roleService.GetOragnizationChartData();
+            return Ok(result);
+        }
     }
 }
