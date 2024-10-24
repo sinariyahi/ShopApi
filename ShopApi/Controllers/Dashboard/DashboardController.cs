aaeecd30ff6f3818ee258f6508e8dd10eb875d45
SinaRiyahi <sina.riyahi76@gmail.com>
2024-10-24 22:52:51 +0330

    Class DashboardController

--- ShopApi/Controllers/Dashboard/DashboardController.cs
+++ ShopApi/Controllers/Dashboard/DashboardController.cs
@@ -1,6 +1,32 @@
-﻿namespace ShopApi.Controllers.Dashboard
+﻿using Application.Interfaces.Dashboard;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApi.Controllers.Dashboard
 {
-    public class DashboardController
+    public class DashboardController : BaseController
     {
+        private readonly IDashboardService dashboardService;
+        public DashboardController(IDashboardService dashboardService)
+        {
+            this.dashboardService = dashboardService;
+        }
+
+        /// <summary>
+        /// تعداد اپشن ها
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("GetCountData")]
+        public async Task<IActionResult> GetCountData()
+        {
+          //  if (UserType == UserType)
+            {
+                var result = await dashboardService.GetCountData();
+                return Ok(result);
+            }
+          //  else { return BadRequest(); }
+
+        }
+
+
     }
 }
