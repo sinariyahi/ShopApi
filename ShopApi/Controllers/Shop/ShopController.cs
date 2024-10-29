daf6c6c892c99ea59484d02674f40ca95b2fa9d3
SinaRiyahi <sina.riyahi76@gmail.com>
2024-10-29 22:52:55 +0330

    Class ShopController

--- ShopApi/Controllers/Shop/ShopController.cs
+++ ShopApi/Controllers/Shop/ShopController.cs
@@ -1,6 +1,47 @@
-﻿namespace ShopApi.Controllers.Shop
+﻿using Application.Interfaces.Shop;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApi.Controllers.Shop
 {
-    public class ShopController
+    public class ShopController : BaseController
     {
+        private readonly IShopService shopService;
+        public ShopController(IShopService shopService)
+        {
+            this.shopService = shopService;
+        }
+
+        /// <summary>
+        ///  لیست استان - شهر - منطقه
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("GetBasicData/{dataType}/{parentId}")]
+        public async Task<IActionResult> GetBasicData(int dataType, int parentId)
+        {
+            var result = await shopService.GetBasicData(dataType, parentId);
+            return Ok(result);
+        }
+
+        /// <summary>
+        ///  اطلاعات قیمت و موجودی کالا
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("GetPartInfo/{partNo}")]
+        public async Task<IActionResult> GetPartInfo(string partNo)
+        {
+            var result = await shopService.GetPartBalanceInfo(partNo);
+            return Ok(result);
+        }
+
+        /// <summary>
+        ///  محله
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("GetParishList/{cityId}/{regionId}/{term?}")]
+        public async Task<IActionResult> GetPartInfo(int cityId, int regionId, string? term)
+        {
+            var result = await shopService.GetParishList(cityId, regionId, term);
+            return Ok(result);
+        }
     }
 }
