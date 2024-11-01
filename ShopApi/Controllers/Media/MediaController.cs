1f7074b0aaad917d2762509a41ee8e8be4a4efc6
SinaRiyahi <sina.riyahi76@gmail.com>
2024-11-01 23:36:54 +0330

    MediaController

--- ShopApi/Controllers/Media/MediaController.cs
+++ ShopApi/Controllers/Media/MediaController.cs
@@ -1,6 +1,24 @@
-﻿namespace ShopApi.Controllers.Media
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApi.Controllers.Media
 {
-    public class MediaController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MediaController : BaseController
     {
+        private readonly IFileService fileService;
+
+        public MediaController(IFileService fileService)
+        {
+            this.fileService = fileService;
+        }
+
+        [HttpPost("Upload")]
+        public async Task<IActionResult> Upload([FromForm] IFormFile file)
+        {
+            var fileSaveUrl = await fileService.SaveFileInDisk(file);
+            return Ok(fileSaveUrl);
+        }
     }
 }
