46513d511f23cdc7870dbcf530dd2ceb8444e9c5
SinaRiyahi <sina.riyahi76@gmail.com>
2024-11-04 23:45:56 +0330

    Class UserBannerController

--- ShopApi/Controllers/Media/Banner/UserBannerController.cs
+++ ShopApi/Controllers/Media/Banner/UserBannerController.cs
@@ -1,6 +1,33 @@
-﻿namespace ShopApi.Controllers.Media.Banner
+﻿using Application.Interfaces.Media;
+using Infrastructure.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApi.Controllers.Media.Banner
 {
-    public class UserBannerController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserBannerController : ControllerBase
     {
+        private readonly IBannerService bannerService;
+        public UserBannerController(IBannerService bannerService)
+        {
+            this.bannerService = bannerService;
+        }
+
+        /// <summary>
+        ///  لیست  banner  
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("GetUserList/{positionPlace}")]
+        public async Task<IActionResult> GetUserList(PositionPlace positionPlace)
+        {
+            var result = await bannerService.GetUserList(positionPlace);
+            return Ok(result);
+        }
+
+
+
+
+
     }
 }
