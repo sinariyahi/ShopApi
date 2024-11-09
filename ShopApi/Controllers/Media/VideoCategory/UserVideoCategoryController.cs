a9e564d1115890d49f3777090adf2e9cacd42647
SinaRiyahi <sina.riyahi76@gmail.com>
2024-11-09 23:06:45 +0330

    Class UserVideoCategoryController

--- ShopApi/Controllers/Media/VideoCategory/UserVideoCategoryController.cs
+++ ShopApi/Controllers/Media/VideoCategory/UserVideoCategoryController.cs
@@ -1,6 +1,48 @@
-﻿namespace ShopApi.Controllers.Media.VideoCategory
+﻿using Application.Interfaces.Media;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApi.Controllers.Media.VideoCategory
 {
-    public class UserVideoCategoryController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserVideoCategoryController : ControllerBase
     {
+        private readonly IVideoCategoryService videoCategoryService;
+
+        public UserVideoCategoryController(IVideoCategoryService videoCategoryService)
+        {
+            this.videoCategoryService = videoCategoryService;
+        }
+
+
+        /// <summary>
+        ///   برگرداندن یک ویدیو 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("GetById/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await videoCategoryService.GetById(id);
+            return Ok(result);
+        }
+
+
+        /// <summary>
+        ///   لیست گروه بندی ویدیو 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("GetUserList")]
+        public async Task<IActionResult> GetUserList()
+        {
+            var result = await videoCategoryService.GetUserList();
+            return Ok(result);
+        }
+
+
+
+
+
+
+
     }
 }
