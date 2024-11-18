74d75a13b12f28a1101d68e2d93c39a679d2f41f
SinaRiyahi <sina.riyahi76@gmail.com>
2024-11-18 23:43:34 +0330

    Class MenuController

--- ShopApi/Controllers/Security/MenuController.cs
+++ ShopApi/Controllers/Security/MenuController.cs
@@ -1,6 +1,40 @@
-﻿namespace ShopApi.Controllers.Security
+﻿using Application.Interfaces.Security;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApi.Controllers.Security
 {
-    public class MenuController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController : BaseController
     {
+        private readonly IMenuService _menuService;
+
+        public MenuController(IMenuService menuService)
+        {
+            _menuService = menuService;
+        }
+
+
+        /// <summary>
+        ///   منو
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("GetMenu")]
+        public async Task<IActionResult> GetMenu()
+        {
+
+            var result = await _menuService.GetMenu();
+            return Ok(result);
+        }
+        /// <summary>
+        ///   دسترسی منوهای کاربر
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("GetMenuAccessForUser")]
+        public async Task<IActionResult> GetMenuAccessForUser()
+        {
+            var result = await _menuService.GetMenuAccessForUser(UserId);
+            return Ok(result);
+        }
     }
 }
