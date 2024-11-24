b3eaf7186c4fe564163ce18e699d7c7a89c74a88
SinaRiyahi <sina.riyahi76@gmail.com>
2024-11-25 00:09:40 +0330

    Class UserCustomerController

--- ShopApi/Controllers/Security/UserCustomerController.cs
+++ ShopApi/Controllers/Security/UserCustomerController.cs
@@ -1,6 +1,44 @@
-﻿namespace ShopApi.Controllers.Security
+﻿using Application.Interfaces.Security;
+using Infrastructure.Models.Customer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApi.Controllers.Security
 {
-    public class UserCustomerController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserCustomerController : ControllerBase
     {
+        private readonly ICustomerService customerService;
+
+        public UserCustomerController(ICustomerService customerService)
+        {
+            this.customerService = customerService;
+        }
+
+
+        /// <summary>
+        ///     ثبت مشتری جدید 
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("AddCustomer")]
+        public async Task<IActionResult> AddCustomer(CustomerInputModel model)
+        {
+            var result = await customerService.AddCustomer(model, false);
+            return Ok(result);
+        }
+
+
+        /// <summary>
+        ///      
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("RegisterCustomer")]
+        public async Task<IActionResult> RegisterCustomer(RegisterCustomerInputModel model)
+        {
+            var result = await customerService.RegisterCustomer(model);
+            return Ok(result);
+        }
+
+
     }
 }
