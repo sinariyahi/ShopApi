9d23566ac77dd6f205725c4bfebe5883c059c7b6
SinaRiyahi <sina.riyahi76@gmail.com>
2024-12-08 21:54:11 +0330

    Class CaptchaController

--- ShopApi/Controllers/Support/CaptchaController.cs
+++ ShopApi/Controllers/Support/CaptchaController.cs
@@ -1,6 +1,52 @@
-﻿namespace ShopApi.Controllers.Support
+﻿using Infrastructure.Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace ShopApi.Controllers.Support
 {
-    public class CaptchaController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CaptchaController : ControllerBase
     {
+        readonly IMemoryCache cache;
+        public CaptchaController(IMemoryCache cache)
+        {
+            this.cache = cache;
+        }
+
+        [HttpGet("{captchaKey}")]
+        public async Task<FileResult> GetCaptchaImage(string captchaKey)
+        {
+            var text = await CustomCaptcha(captchaKey);
+            if (text == null) return null;
+
+
+            RandomImage ci = new RandomImage(text, 100, 40);
+            var ms = new MemoryStream();
+            ci.Image.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            ci.Image.Dispose();
+            return File(ms.ToArray(), "image/jpeg");
+        }
+        async Task<string> CustomCaptcha(string captchaName)
+        {
+            var generateRandomCode = GenerateRandomCode();
+
+            cache.Remove(captchaName);
+            var code = await cache.GetOrCreateAsync(captchaName, entry => { entry.SetAbsoluteExpiration(DateTime.Now.AddMinutes(30)); return generateRandomCode; });
+            return code;
+        }
+        Task<string> GenerateRandomCode()
+        {
+            Random r = new Random();
+            string s = "";
+            for (int j = 0; j < 4; j++)
+            {
+                int ch = r.Next(0, 9);
+                s = s + ch.ToString();
+                r.NextDouble();
+                r.Next(100, 1999);
+            }
+            return Task.FromResult(s);
+        }
     }
 }
