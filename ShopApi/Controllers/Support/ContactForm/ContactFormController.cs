fd544a930dea957506f9a011faada349943f5c4a
SinaRiyahi <sina.riyahi76@gmail.com>
2024-12-13 23:49:42 +0330

    Class ContactFormController

--- ShopApi/Controllers/Support/ContactForm/ContactFormController.cs
+++ ShopApi/Controllers/Support/ContactForm/ContactFormController.cs
@@ -1,6 +1,46 @@
-﻿namespace ShopApi.Controllers.Support.ContactForm
+﻿using Application.Interfaces.Support;
+using Infrastructure.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShopApi.Controllers.Support.ContactForm
 {
-    public class ContactFormController
-    {
+        public class ContactFormController : BaseController
+        {
+            private readonly IContactFormService service;
+            public ContactFormController(IContactFormService service)
+            {
+                this.service = service;
+            }
+            /// <summary>
+            ///  لیست  همکاری با ما ها 
+            /// </summary>
+            /// <returns></returns>
+            [HttpPost("GetList")]
+            public async Task<IActionResult> GetList(GridQueryModel model = null)
+            {
+         //       if (UserType == UserType)
+        //        {
+                    var result = await service.GetList(model);
+                    return Ok(result);
+        //        }
+        //        else { return BadRequest(); }
+
+            }
+
+
+            /// <summary>
+            ///   برگرداندن جزییات همکاری با ما 
+            /// </summary>
+            /// <returns></returns>
+            [HttpGet("GetById/{id}")]
+            public async Task<IActionResult> GetById(int id)
+            {
+                var result = await service.GetById(id);
+                return Ok(result);
+            }
+
+
+
+        }
     }
 }
