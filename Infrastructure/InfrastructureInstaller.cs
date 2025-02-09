7f1c2f44bdb3c17b7704bad160dde81ceb0aa433
SinaRiyahi <sina.riyahi76@gmail.com>
2025-02-10 00:32:24 +0330

    InfrastructureInstaller

--- Infrastructure/InfrastructureInstaller.cs
+++ Infrastructure/InfrastructureInstaller.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Infrastructure.Common;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,36 @@ using System.Threading.Tasks;
 
 namespace Infrastructure
 {
-    internal class InfrastructureInstaller
+    public static class InfrastructureInstaller
     {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddScoped<IConnectionUtility, ConnectionUtility>();
+            services.AddScoped<ICustomEncryption, CustomEncryption>();
+            services.AddSingleton<MailUtility>();
+            services.AddSingleton<SMSUtility>();
+
+        //    var sp = services.BuildServiceProvider();
+        //    Configs configs = sp.GetService<IOptions<Configs>>().Value;
+
+        //    services.AddMailKit(optionBuilder =>
+        //    {
+        //        optionBuilder.UseMailKit(new MailKitOptions()
+        //        {
+        //            //get options from sercets.json
+        //            Server = configs.MailServer,
+        //            Port = configs.Port,
+        //            SenderName = configs.ConfirmationEmail,
+        //            SenderEmail = configs.ConfirmationEmail,
+        //            // can be optional with no authentication 
+        //            Account = configs.ConfirmationEmail,
+        //            //Password = configs.ConfirmationEmailPassword,
+        //            // enable ssl or tls
+        //            Security = false,
+        //        });
+        //    });
+
+            return services;
+        }
     }
 }
