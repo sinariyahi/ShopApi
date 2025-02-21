0b4788dbbf4f93a6bb9fa0b571a3fd38f81531f3
SinaRiyahi <sina.riyahi76@gmail.com>
2025-02-21 23:42:17 +0330

    FileType

--- Domain/Entities/Support/CooperationForm.cs
+++ Domain/Entities/Support/CooperationForm.cs
@@ -7,6 +7,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Infrastructure.Common;
 
 namespace Domain.Entities.Support
 {
@@ -47,7 +48,7 @@ namespace Domain.Entities.Support
         public string FileExtension { get; set; }
         public string FileContentType { get; set; }
         public string FileSize { get; set; }
-     //   public FileType FileType { get; set; }
+        public FileType FileType { get; set; }
         public string FilePath { get; set; }
         public DateTime CreateDate { get; set; }
         public int CooperationFormId { get; set; }
